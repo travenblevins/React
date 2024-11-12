3a4c4675d0234487f7ba1ec430f2f4a01ce14971
Traven <TRAVEN.BLEVINS9061@STU.MTEC.EDU>
2024-11-12 07:35:14 -0700

    user update

--- user-interface/src/app/page.js
+++ user-interface/src/app/page.js
@@ -4,23 +4,20 @@ import { useContext } from "react";
 import Header from "./components/header.js";
 import Footer from "./components/footer";
 import Message from "./components/message";
-
-import { AuthContext, AuthProvider } from "./context/AuthContext.js";
+import { AuthProvider, AuthContext } from "./context/AuthContext.js";
 
 export default function Home() {
   const user = useContext(AuthContext);
 
   return (
-    <AuthContext.Provider value={user}>
-      <AuthProvider>
-        <div className="flex flex-col w-full h-screen">
-          <Header />
-          <main className="flex flex-grow justify-center pt-5">
-            <Message />
-          </main>
-          <Footer />
-        </div>
-      </AuthProvider>
-    </AuthContext.Provider>
+    <AuthProvider>
+      <div className="flex flex-col w-full h-screen">
+        <Header />
+        <main className="flex flex-grow justify-center pt-5">
+          <Message />
+        </main>
+        <Footer />
+      </div>
+    </AuthProvider>
   );
 }
